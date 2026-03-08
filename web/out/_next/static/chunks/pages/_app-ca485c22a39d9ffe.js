dfc6934b843c82cab57f5907a7ae28b945f12634
seannyquest <seannyquest@users.noreply.github.com>
2026-03-08 15:33:37 -0500

    Static export with direct OpenF1 API, deployed to GitHub Pages

---
+++ web/out/_next/static/chunks/pages/_app-ca485c22a39d9ffe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{78121:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(36719)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(78121),_(14745)}),_N_E=n.O()}]);
